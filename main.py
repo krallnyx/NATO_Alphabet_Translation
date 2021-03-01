3a39d336534c04a74bf768f23c9250e66290f8c4
Arnaud Jeanroch <arnaud@jeanroch.fr>
2021-03-01 11:20:16 +0000

    #1 added an error handling using recursivity

--- main.py
+++ main.py
@@ -4,6 +4,15 @@ data = pandas.read_csv("nato_phonetic_alphabet.csv")
 alphabet = {row.letter: row.code for (index, row) in data.iterrows()}
 
 
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-to_translate = input("Please enter the word you want to translate into NATO Alphabet.\n").upper()
-print([alphabet[letter] for letter in to_translate])
+def generate_phonetic():
+    to_translate = input("Please enter the word you want to translate into NATO Alphabet.\n").upper()
+    try:
+        output = [alphabet[letter] for letter in to_translate]
+    except KeyError:
+        print("Please enter a valid word, only letters in the alphabet are allowed")
+        generate_phonetic()
+    else:
+        print(output)
+
+
+generate_phonetic()
